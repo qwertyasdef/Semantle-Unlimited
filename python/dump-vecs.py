f4c79c26fc229d653281f3d65313c7fb585860d7
Brian Yao <byao9310@gmail.com>
2022-03-15 06:55:28 -0700

    make sure to actually commit when dumping vectors, and dump to three separate files so it doesn't exceed the 100 Mb limit

--- python/dump-vecs.py
+++ python/dump-vecs.py
@@ -21,33 +21,44 @@ def bfloat(vec):
     return vec[1::2].tobytes()
 
 
-# many weird words contain #, _ for multi-word
-# some have e-mail addresses, start with numbers, :-), lots of === signs, ...
-def filter_vocab(vocab):
-    return [word for word in vocab if word.isalpha() and word.islower()]
-
-
 if __name__ == '__main__':
     model = word2vec.load("GoogleNews-vectors-negative300.bin", encoding="ISO-8859-1", new_lines=False)
 
-    con = sqlite3.connect("../data/word2vec.db")
+    # 3 database files to get around github's 100 Mb file limit
+    con1 = sqlite3.connect("../data/word2vec_a-i.db")
+    con2 = sqlite3.connect("../data/word2vec_j-r.db")
+    con3 = sqlite3.connect("../data/word2vec_s-z.db")
+
+    for con in (con1, con2, con3):
+        con.execute("PRAGMA journal_mode=WAL")
+        cur = con.cursor()
+        cur.execute("""create table if not exists word2vec (word text PRIMARY KEY, vec blob)""")
+        con.execute("DELETE FROM word2vec")
+        con.commit()
+
     file = open("../data/words.txt", "w")
 
-    con.execute("PRAGMA journal_mode=WAL")
-    cur = con.cursor()
-    cur.execute("""create table if not exists word2vec (word text PRIMARY KEY, vec blob)""")
-    con.commit()
+    # Write to database and words file
+    for word in tqdm.tqdm(model.vocab):
+        # many weird words contain #, _ for multi-word
+        # some have e-mail addresses, start with numbers, :-), lots of === signs, ...
+        if not word.isalpha() or not word.islower():
+            continue
 
-    # import pdb;pdb.set_trace()
+        if word[0] in "abcdefghi":
+            con = con1
+        elif word[0] in "jklmnopqr":
+            con = con2
+        else:  # stuvwxyz
+            con = con3
 
-    CHUNK_SIZE = 1111
-    con.execute("DELETE FROM word2vec")
-    for words in chunked(tqdm.tqdm(filter_vocab(model.vocab)), CHUNK_SIZE):
-        con.executemany(
+        con.execute(
             "insert into word2vec values(?,?)",
-            ((word, bfloat(model[word])) for word in words),
+            (word, bfloat(model[word])),
         )
-        file.writelines(word + "\n" for word in words)
+        file.write(word + "\n")
 
-    con.close()
+    for con in (con1, con2, con3):
+        con.commit()
+        con.close()
     file.close()
